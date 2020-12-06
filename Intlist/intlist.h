a8fd967aced009afd85637cc94e2f942f4e59d61
Robin Willocquet <robin.willocquet@mines-paristech.fr>
2020-12-06 12:39:54 +0100

    Ajout de Intlist pour le 9 décembre

---
+++ Intlist/intlist.h
@@ -0,0 +1,133 @@
+#include <iostream>
+
+class IntCell {
+
+friend class IntList;
+
+private:
+    IntCell () : PrevCell(NULL), NextCell(NULL), val(0) {
+
+     }
+    IntCell *PrevCell;
+    IntCell *NextCell;
+    int val;
+
+};
+
+class IntList {
+public:
+    IntList () : FirstCell(NULL), LastCell(NULL) {
+
+     }
+
+    ~IntList () {
+        FirstCell = NULL;
+        LastCell = NULL;
+    }
+
+    void add_front(int a) {
+        IntCell *front = new IntCell;
+        front -> val = a;
+        if(FirstCell != NULL) {
+            FirstCell -> PrevCell = front;
+            front -> NextCell = FirstCell;
+        }
+        FirstCell = front;
+        if(LastCell == NULL) {
+            LastCell = front;
+        }
+    }
+
+    void add_back(int a) {
+        IntCell *back = new IntCell;
+        back -> val = a;
+        if(LastCell != NULL) {
+            LastCell -> NextCell = back;
+            back -> PrevCell = LastCell;
+        }
+        LastCell = back;
+        if(FirstCell == NULL) {
+            FirstCell = back;
+        }
+    }
+
+    void remove_front() {
+        if(FirstCell != NULL) {
+            IntCell* first = FirstCell;
+            FirstCell = first -> NextCell;
+            FirstCell -> PrevCell = NULL;
+            first -> NextCell = NULL;
+        }
+    }
+    
+    void remove_back() {
+        if(LastCell != NULL) {
+            IntCell* last = LastCell;
+            LastCell = last -> PrevCell;
+            LastCell -> NextCell = NULL;
+            last -> PrevCell = NULL;
+        }
+    }
+
+    void remove(int a) {
+        if(search(a)) {
+            IntCell* top = FirstCell;
+            if(top -> val == a) {
+                remove_front();
+            }
+            else {
+                while(top -> NextCell != NULL) {
+                    top = top -> NextCell;
+                    if(top -> val == a) {
+                        if(top -> NextCell != NULL) {
+                            IntCell* prev = top -> PrevCell;
+                            IntCell* next = top -> NextCell;
+                            prev -> NextCell = next;
+                            next -> PrevCell = prev;
+                            top -> NextCell = NULL;
+                            top -> PrevCell = NULL;
+                        }
+                        else {
+                            remove_back();
+                        }
+
+                    }
+                }
+            }
+        }
+
+    }
+
+    bool search(int a) {
+        IntCell* top = FirstCell;
+        if(top -> val == a) {
+            return true;
+        }
+        while(top -> NextCell != NULL) {
+            top = top -> NextCell;
+            if(top -> val == a) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void print() {
+        std::cout << '{';
+        IntCell* top = FirstCell;
+        std::cout << top -> val << ' ';
+        while(top -> NextCell != NULL) {
+            top = top -> NextCell;
+            std::cout << top -> val << ' ';
+        }
+        std::cout << '}' << std::endl;
+    }
+
+    
+
+private:
+    IntCell *FirstCell;
+    IntCell *LastCell;
+
+};
+
